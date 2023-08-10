78a21fa140dc0d1fb6735bf8abc8f3efb98ac7ae
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-08-10 09:54:57 -0500

    gradiente implementado y funcional

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:videos_tiktok_app/presentation/widgets/video/video_background.dart';
 
 class FullScreenPlayer extends StatefulWidget {
   //mirar Video_Scrollable_view.dar de ahi se manda la url del video
@@ -44,13 +45,34 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
               ),
             );
           }
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                VideoPlayer(controller),
-                Positioned(bottom: 50, left: 20, child: _VideoCaption(caption: widget.caption))
-              ],
+          // detector de gestos
+          return GestureDetector(
+            // darle tap a la pantalla para que se pause
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+                return;
+              }
+
+              controller.play();
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  // reproduccion del video
+                  VideoPlayer(controller),
+
+                  // gradiente
+                  VideoBackground(stops: const [0.5, 1.0],),
+
+                  // texto del video
+                  Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: _VideoCaption(caption: widget.caption))
+                ],
+              ),
             ),
           );
         });
@@ -59,15 +81,21 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
 
 class _VideoCaption extends StatelessWidget {
   final String caption;
-  const _VideoCaption({super.key, required this.caption});
+  const _VideoCaption({required this.caption});
 
   @override
   Widget build(BuildContext context) {
+    //para agarrar el tamaño de la pantalla
     final size = MediaQuery.of(context).size;
+    // el estilo del titulo
     final titleStyle = Theme.of(context).textTheme.titleLarge;
     return SizedBox(
       width: size.width * 0.6,
-      child: Text( caption, maxLines: 2, style: titleStyle,),
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
